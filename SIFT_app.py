2879052b3d89ac12e00a64f954505bdd760e58b3
fizzer <jed324@gmail.com>
2020-10-02 20:34:55 -0700

    homography tracking finished

--- SIFT_app.py
+++ SIFT_app.py
@@ -67,27 +67,40 @@ class My_App(QtWidgets.QMainWindow):
 
         # FLANN parameters
         FLANN_INDEX_KDTREE = 0
+        MIN_MATCH_COUNT = 10
         index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
         search_params = dict(checks=50)
 
-        flann = cv2.FlannBasedMatcher(index_params, search_params)
-
-        matches = flann.knnMatch(des_img, des_cam, k=2)
+        # FLANN 
+        matches = cv2.FlannBasedMatcher(index_params, search_params).knnMatch(des_img, des_cam, k=2)
         matches_mask = [[0,0] for i in xrange(len(matches))]
 
+        good = []
         # apply Lowe's ratio test
         for i,(m,n) in enumerate(matches):
             if m.distance < 0.7 * n.distance:
-                matches_mask[i]=[1,0]
-
+                matches_mask[i] = [1,0]
+                good.append(m)
+        
         draw_params = dict(matchColor = (0,255,0),
                    singlePointColor = (255,0,0),
                    matchesMask = matches_mask,
                    flags = 0)
 
-        out = cv2.drawMatchesKnn(img, kp_img, frame, kp_cam, matches, None, **draw_params)
+        if len(good) > MIN_MATCH_COUNT:
+            src_pts = np.float32([ kp_img[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+            dst_pts = np.float32([ kp_cam[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
 
+            matrix, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
 
+            h, w = img_gray.shape
+            pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+            dst = cv2.perspectiveTransform(pts, matrix)
+
+            out = cv2.polylines(frame, [np.int32(dst)], True, 255,3, cv2.LINE_AA)
+
+        else:
+            out = cv2.drawMatchesKnn(img, kp_img, frame, kp_cam, matches, None, **draw_params)
 
         # Display image on webcam
         pixmap = self.convert_cv_to_pixmap(out)
